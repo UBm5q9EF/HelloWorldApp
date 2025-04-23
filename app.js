8a1ab003588df01ca2a81e75e0748bab61d5c9c7
Abhishek Mane <anirudhmane956@gmail.com>
2025-04-23 13:23:46 +0530

    Updated files

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
   res.send('Hello from EC2 Node.js!');
 });
-
-const port = 3000;
-const host = '0.0.0.0'; // Required for EC2 to allow external access
-
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
+app.listen(3000, () => {
+  console.log('Server running at port 3000');
 });
