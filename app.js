2c51be2e2c5d8f97fad9a07d1a8aac16f1b978eb
Abhishek Mane <anirudhmane956@gmail.com>
2025-04-23 17:03:46 +0530

    Test CI/CD deployment

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// app.js
+
 const express = require('express');
 const app = express();
+const PORT = 3000;
+
+// Root route
 app.get('/', (req, res) => {
   res.send('Hello from EC2 Node.js!');
 });
-app.listen(3000, () => {
-  console.log('Server running at port 3000');
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
